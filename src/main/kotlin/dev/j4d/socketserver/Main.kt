ec08bef9b60fe6c8a6dc88cdda406bc3771d1240
JarandDev <jarand95@gmail.com>
2025-11-05 20:31:29 +0100

    New version

--- src/main/kotlin/dev/j4d/socketserver/Main.kt
+++ src/main/kotlin/dev/j4d/socketserver/Main.kt
@@ -5,6 +5,8 @@ import dev.j4d.socketserver.clientlistener.ClientListener
 fun main(args: Array<String> = emptyArray()) {
     val arguments = args.associate { it.split("=")[0] to it.split("=")[1] }
 
+    println("New version")
+
     val clientListener = ClientListener(
         acceptPort = arguments["acceptPort"]?.toInt() ?: 8080,
         acceptTimeout = arguments["acceptTimeout"]?.toLong() ?: 60_000L,
